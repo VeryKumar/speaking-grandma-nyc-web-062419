4f6a688683dd07bb501f4a39a9f0df01e0914dfb
Varun <uberkumar@gmail.com>
2019-06-19 03:00:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
-speak_to_grandma(string)
+def speak_to_grandma(string)
   if string != string.uppercase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.uppercase
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"
+end
