76bf1018202eb9f43bd30cc6a46647f2c4d7ba28
Varun <uberkumar@gmail.com>
2019-06-19 02:54:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,4 +3,5 @@ speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.uppercase
     return "NO, NOT SINCE 1938!"
-  
+  elsif string == "I LOVE YOU GRANDMA"
+    return "I LOVE "
